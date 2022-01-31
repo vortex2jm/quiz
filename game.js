6359b4daf6be39763a58fe3d89755a8fcdb64321
vortex2jm <joaopaulomoura5328@gmail.com>
2022-01-31 12:22:39 -0300

    progressão de perguntas funcionando

--- game.js
+++ game.js
@@ -1,13 +1,13 @@
 
 //Questões pré-escritas (posteriormente serão adicionadas a um JSON)
-let questions = [
+const questions = [
     {
         question: "qual foi o último ano que o Brasil ganhou uma copa do mundo?",
         answer1: "2006",
         answer2: "2010",
         answer3: "1998",
         answer4: "2002",
-
+        right: "2002",
     },
     {
         question: "Quem foi campeão do major de CSGO em 2021?",
@@ -15,6 +15,7 @@ let questions = [
         answer2: "G2",
         answer3: "Vitality",
         answer4: "NIP",
+        right: "Navi"
     },
     {
         question: "Quem descreveu a teoria da relatividade?",
@@ -22,35 +23,78 @@ let questions = [
         answer2: "Tesla",
         answer3: "Newton",
         answer4: "Rutherford",
+        right: "Einstein"
     }
 ]
-    //perguntas e respostas
-    const question = document.querySelector('#question_text');
-    const answer1 = document.querySelector('#a');
-    const answer2 = document.querySelector('#b');
-    const answer3 = document.querySelector('#c');
-    const answer4 = document.querySelector('#d');
-    //foguete
-    const overbox = document.querySelector("#overbox"); 
 
-/*var x = 0;
+const preQuestions = [{
 
-    setQuestion(questions, 0);
-    const gameInterval = setInterval(setQuestion, 10000, questions, x);
+    question: "Você tem X segundos para resolver as questões seguintes, está preparado?",
+    answer1: "...",
+    answer2: "...",
+    answer3: "...",
+    answer4: "...",
+}]
 
-    setTimeout(()=> {
-        clearTimeout(gameInterval);
-        console.log(x);
-    }, 50000);
+//perguntas e respostas
+const question = document.querySelector('#question_text');
+const answer1 = document.querySelector('#a');
+const answer2 = document.querySelector('#b');
+const answer3 = document.querySelector('#c');
+const answer4 = document.querySelector('#d');
+//foguete
+const overbox = document.querySelector("#overbox");
+//caixa de perguntas
+const questionBox = document.querySelector('#question');
+//seleção todos os botões
+const answers = document.querySelectorAll('.alternatives');
 
-    console.log(x);
+//GAME
+
+let timeFlag = questions.length;    //quantidade de questões
+let stopFlag = 0;   //flag de parada
+//console.log(timeFlag);
+let qNumber = 0;    //quantidade de questões
+let animationTime = timeFlag*5 + 's';   //tempo da animação concatenado com "s" para settar o CSS
+//console.log(ani);
+
+
+    answers.disabled = true;
+    setQuestion(preQuestions,0);
+
+    setTimeout(() => {
+        setQuestion(questions,qNumber);
+        answers.disabled = false;
+        overbox.style.animationDuration = animationTime;
+        overbox.style.animationName = 'flyGo';
+    },3000); //mudar 3000 para 10000
+
+    setTimeout(() => {
+        stopFlag = 1;
+    }, 5000 * timeFlag);
     
-    ******Não funciona porque a função setInterval pega a função com os mesmos parâmetros sempre*****
+    
+    //questionBox.className = 'alertQuestion';
+    //questionBox.classList.remove("alertQuestion");
+    //question.style.color = "black";
 
-    */
+    
+    setTimeout(() => {
+
+        for(let i =0; i<questions.length - 1; i++){
+            
+            setQuestion(questions, qNumber);
+
+            
+        }
 
 
-    setQuestion(questions, 0);
+    },15000);
+
+    
+
+
+
 
 
 //___________________________________FUNÇÕES________________________________// 
@@ -64,15 +108,12 @@ function flyback(){
     console.log('set rocket position');
 }
 
-
-
 //Função para setar uma pergunta no HTML
 
 function setQuestion(objectQuestion, questioNumber){
 
     console.log(objectQuestion);
 
-    setTimeout(flyback, 10000);
 
     question.textContent = objectQuestion[questioNumber].question;
     answer1.textContent = objectQuestion[questioNumber].answer1;
@@ -81,5 +122,6 @@ function setQuestion(objectQuestion, questioNumber){
     answer4.textContent = objectQuestion[questioNumber].answer4;
 
     //console.log('valor de x = ' + x);
-    console.log('valor de quetioNumber = ' + questioNumber);
+    qNumber += 1;
+    console.log('valor de questioNumber = ' + questioNumber);
 }
---
+++ gameS.js
@@ -0,0 +1,132 @@
+const questions = [
+    {
+        question: "qual foi o último ano que o Brasil ganhou uma copa do mundo?",
+        answer1: "2006",
+        answer2: "2010",
+        answer3: "1998",
+        answer4: "2002",
+        right: "2002",
+    },
+    {
+        question: "Quem foi campeão do major de CSGO em 2021?",
+        answer1: "Navi",
+        answer2: "G2",
+        answer3: "Vitality",
+        answer4: "NIP",
+        right: "Navi"
+    },
+    {
+        question: "Quem descreveu a teoria da relatividade?",
+        answer1: "Einstein",
+        answer2: "Tesla",
+        answer3: "Newton",
+        answer4: "Rutherford",
+        right: "Einstein"
+    }
+]
+
+const preQuestions = [{
+
+    question: "Você tem X segundos para resolver as questões seguintes, está preparado?",
+    answer1: "",
+    answer2: "",
+    answer3: "",
+    answer4: "",
+    },
+    {
+        question: "Parabéns, você terminou o quiz!!!",
+        answer1: "",
+        answer2: "",
+        answer3: "",
+        answer4: "", 
+    }
+]
+
+const question = document.querySelector('#question_text');
+const answer1 = document.querySelector('#a');
+const answer2 = document.querySelector('#b');
+const answer3 = document.querySelector('#c');
+const answer4 = document.querySelector('#d');
+const overbox = document.querySelector("#overbox");
+const questionBox = document.querySelector('#question');
+const answers = document.getElementsByClassName('alternatives');
+
+let qNumber = -1; //setado em -1 por conta da pré questão
+let checkCounter = 0;   //quantidade de questões respondidas
+let totalQuestions = questions.length;  //quantidade total de questões
+let totalTime = totalQuestions * 5; //tempo que o usuário terá para responder as perguntas
+
+function setQuestion(objectQuestion, questioNumber){
+
+    console.log(objectQuestion);
+
+    question.textContent = objectQuestion[questioNumber].question;
+    answer1.textContent = objectQuestion[questioNumber].answer1;
+    answer2.textContent = objectQuestion[questioNumber].answer2;
+    answer3.textContent = objectQuestion[questioNumber].answer3;
+    answer4.textContent = objectQuestion[questioNumber].answer4;
+
+    //console.log('valor de x = ' + x);
+    if(qNumber < questions.length - 1){qNumber += 1;};
+    console.log('valor de questioNumber = ' + questioNumber);
+}
+
+//___________________________________________________________________________________//
+//___________________________________________________________________________________//
+
+    preQuestions[0].question = "Você tem "+totalTime+" segundos para resolver as seguintes questões. Preparado?";
+    setQuestion(preQuestions,0);
+    questionBox.className = "alertQuestion";
+    
+    setTimeout(() => {  
+        
+        answer1.style.backgroundImage = "none";
+        answer2.style.backgroundImage = "none";
+        answer3.style.backgroundImage = "none";
+        answer4.style.backgroundImage = "none";
+        //questionBox.style.backgroundImage = "none";
+        questionBox.classList.remove("alertQuestion");
+        overbox.style.animationName = "flyGo";
+        overbox.style.animationDuration = totalTime + "s";
+
+        setQuestion(questions,qNumber);
+        let a = answer1.addEventListener("click", function(){
+            setQuestion(questions,qNumber);
+            checkCounter += 1;
+            if(checkCounter == totalQuestions){
+                
+                window.location.href = "./final.html";
+            }
+        });
+        let b = answer2.addEventListener("click", function(){
+            setQuestion(questions,qNumber);
+            checkCounter += 1;
+            if(checkCounter == totalQuestions){
+               
+                window.location.href = "./final.html";
+            }
+        });
+        let c = answer3.addEventListener("click", function(){
+            setQuestion(questions,qNumber);
+            checkCounter += 1;
+            if(checkCounter == totalQuestions){
+               
+                window.location.href = "./final.html";
+            }
+        });
+        let d = answer4.addEventListener("click", function(){
+            setQuestion(questions,qNumber);
+            checkCounter += 1;
+            if(checkCounter == totalQuestions){
+                
+                window.location.href = "./final.html";
+            }
+        });
+
+    }, 5000);
+
+    
+
+
+
+
