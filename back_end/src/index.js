d8526ace5075ae8439ac7467b51de61a0407684d
vortex2jm <joaopaulomoura5328@gmail.com>
2022-05-11 00:27:23 -0300

    adding knex and sqlite3 driver

--- back_end/src/index.js
+++ back_end/src/index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const routes = require("./routes");
 const cors = require("cors");
+
+
 const app = express();
 
-app.use(cors());
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
+
 const port = 3003;
+
 app.listen(port, ()=> {
-    
     console.log("Running on port " + port);
 })
--- back_end/src/routes.js
+++ back_end/src/routes.js
@@ -1,15 +1,20 @@
-const { Router, response } = require("express");
-const routes = Router();
+const express = require("express");
+const routes = express.Router();
 
-const data = require("./../data/quest.json");
-const data2 = require("./../data/results.json");
+
+
+
 
 routes.get("/",(req,res)=>{
-    res.json(data);
+
+    
 })
 
+
 routes.get("/results",(req,res)=>{
-    res.json(data2);
+
+    
+
 })
 
 module.exports = routes;
