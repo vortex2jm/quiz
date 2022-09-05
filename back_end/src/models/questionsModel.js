356d4ef36f41464dd3bd6470dad760da3d263ce4
vortex2jm <joaopaulomoura5328@gmail.com>
2022-09-05 04:58:41 -0300

    :sparkles: Adding entity layer

---
+++ back_end/src/models/questionsModel.js
@@ -0,0 +1,10 @@
+module.exports = class Questions {
+    constructor(question, answer1, answer2, answer3, answer4, right){
+        this.question = question;
+        this.answer1 = answer1;
+        this.answer2 = answer2;
+        this.answer3 = answer3;
+        this.answer4 = answer4;;
+        this.right = right;
+    }
+}
