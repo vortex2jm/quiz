056210cfe302df90989fab32affbe656c4088837
vortex2jm <joaopaulomoura5328@gmail.com>
2022-09-05 04:59:47 -0300

    :recycle: Adding model into controller layer

--- back_end/src/controllers/questionsController.js
+++ back_end/src/controllers/questionsController.js
@@ -1,12 +1,21 @@
 const QuestionServices = require("../services/questionsServices")
- 
+const QuestionModel = require("../models/questionsModel");
+
 module.exports = class QuestionsController {
 
   async createQuestion(req, res){
     const qstSvc = new QuestionServices()
-    
+    const qst = new QuestionModel(
+      req.body.question,
+      req.body.answer1,
+      req.body.answer2,
+      req.body.answer3,
+      req.body.answer4,
+      req.body.right
+    )
+
     try{
-      const id = await qstSvc.create(req.body)
+      const id = await qstSvc.create(qst)
       return res.json({id})
 
     }catch(error){
